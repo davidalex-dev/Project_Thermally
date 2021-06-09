d7faa06e692a5c2a79ac81ac09f9d3b59b6eef48
David Alexander <dalexander03@student.ciputra.ac.id>
2021-06-09 13:56:22 +0700

    More design tidyup, final code for now

--- app/src/main/java/com/uc/thermally/HistoryActivity.java
+++ app/src/main/java/com/uc/thermally/HistoryActivity.java
@@ -1,6 +1,7 @@
 package com.uc.thermally;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,39 +17,63 @@ import java.util.ArrayList;
 public class HistoryActivity extends AppCompatActivity {
 
     private RecyclerView history_recyclerView;
-    private static final String TAG = "ConvertInfoActivity";
+    private static final String TAG = "HistoryActivity";
     public static ArrayList<History> listTemp = new ArrayList<>();
     private HistoryRVAdapter adapter = new HistoryRVAdapter(listTemp);
     private TextView history_noData;
+    private Toolbar history_Toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "super on create saved instance state");
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "setContentView activity history");
         setContentView(R.layout.activity_history);
+        Log.d(TAG, "entering initview");
         initView();
+        Log.d(TAG, "entering setuprecyclerview");
         setupRecyclerView();
+
         //addTemp(); //for debug purposes only
 
+        Log.d(TAG, "listTemp size is: " + listTemp.size());
         if(listTemp.size() < 1){
+            Log.d(TAG, "no data set visible");
             history_noData.setVisibility(View.VISIBLE);
         }else{
+            Log.d(TAG, "nodata ser invinsible");
             history_noData.setVisibility(View.INVISIBLE);
+            adapter.notifyDataSetChanged();
+            Log.d(TAG, "adapter notifydatasetchanged");
         }
+
+        history_Toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "clicked on back button");
+                finish();
+            }
+        });
 
     }
 
     private void initView(){
         ConvertActivity ca = new ConvertActivity();
+        Log.d(TAG, "convert ca new converactvitiy");
         listTemp = ca.getList();
+        Log.d(TAG, "get list");
         history_recyclerView = findViewById(R.id.history_recyclerView);
+        Log.d(TAG, "find view by id recyclerview");
         history_noData = findViewById(R.id.history_noData);
+        Log.d(TAG, "findviewbyid nodata");
+        history_Toolbar = findViewById(R.id.history_Toolbar);
+        Log.d(TAG, "findviewbyid toolbar");
     }
 
     private void setupRecyclerView(){
         RecyclerView.LayoutManager manager = new LinearLayoutManager(getBaseContext());
         history_recyclerView.setLayoutManager(manager);
         history_recyclerView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
     }
 
     private void addTemp(){
