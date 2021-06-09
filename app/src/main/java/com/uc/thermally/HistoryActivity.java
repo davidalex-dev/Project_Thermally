b5852ed52c8cf91bf23f91f2885ad9a03ea56164
David Alexander <dalexander03@student.ciputra.ac.id>
2021-06-09 15:49:55 +0700

    HistoryActivity is fixed, fixed final code

--- app/src/main/java/com/uc/thermally/HistoryActivity.java
+++ app/src/main/java/com/uc/thermally/HistoryActivity.java
@@ -4,53 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.uc.thermally.model.History;
-
 import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
 
     private RecyclerView history_recyclerView;
-    private static final String TAG = "HistoryActivity";
-    public static ArrayList<History> listTemp = new ArrayList<>();
-    private HistoryRVAdapter adapter = new HistoryRVAdapter(listTemp);
+    private static final String TAG = "ConvertInfoActivity";
+    private ArrayList<History> listTemp = new ArrayList<History>();
+    private ArrayList<History> listHistory = new ArrayList<History>();
+    private HistoryRVAdapter adapter = new HistoryRVAdapter(listHistory);
     private TextView history_noData;
     private Toolbar history_Toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "super on create saved instance state");
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "setContentView activity history");
         setContentView(R.layout.activity_history);
-        Log.d(TAG, "entering initview");
         initView();
-        Log.d(TAG, "entering setuprecyclerview");
         setupRecyclerView();
-
         //addTemp(); //for debug purposes only
 
-        Log.d(TAG, "listTemp size is: " + listTemp.size());
-        if(listTemp.size() < 1){
-            Log.d(TAG, "no data set visible");
-            history_noData.setVisibility(View.VISIBLE);
-        }else{
-            Log.d(TAG, "nodata ser invinsible");
+        ConvertActivity ca = new ConvertActivity();
+        listTemp = ca.getList();
+        Log.d(TAG, "Listtemp size: " + String.valueOf(listTemp.size()));
+
+        if(listTemp.size()>0){
             history_noData.setVisibility(View.INVISIBLE);
+            for(int i=0; i < listTemp.size(); i++){
+                listHistory.add(new History(listTemp.get(i).getTemp(), listTemp.get(i).getType(), listTemp.get(i).getCondition()));
+            }
             adapter.notifyDataSetChanged();
-            Log.d(TAG, "adapter notifydatasetchanged");
+        }else{
+            history_noData.setVisibility(View.VISIBLE);
         }
 
-        history_Toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        history_Toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "clicked on back button");
                 finish();
             }
         });
@@ -58,16 +54,9 @@ public class HistoryActivity extends AppCompatActivity {
     }
 
     private void initView(){
-        ConvertActivity ca = new ConvertActivity();
-        Log.d(TAG, "convert ca new converactvitiy");
-        listTemp = ca.getList();
-        Log.d(TAG, "get list");
-        history_recyclerView = findViewById(R.id.history_recyclerView);
-        Log.d(TAG, "find view by id recyclerview");
         history_noData = findViewById(R.id.history_noData);
-        Log.d(TAG, "findviewbyid nodata");
         history_Toolbar = findViewById(R.id.history_Toolbar);
-        Log.d(TAG, "findviewbyid toolbar");
+        history_recyclerView = findViewById(R.id.history_recyclerView);
     }
 
     private void setupRecyclerView(){
@@ -77,10 +66,8 @@ public class HistoryActivity extends AppCompatActivity {
     }
 
     private void addTemp(){
-        listTemp.add(new History(69.4, "C", "Hot"));
-        listTemp.add(new History(42.0, "C", "Hot"));
+        listHistory.add(new History(20.0, "a", "b"));
         adapter.notifyDataSetChanged();
     }
-
 
 }
